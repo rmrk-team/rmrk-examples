f8e07d447dae67e59f210d1cc0fd1c2f1297092c
steven2308 <steven2308@outlook.com>
2023-09-08 11:54:13 -0500

    Adds autoaccept functionality.

--- contract-examples/chunkies/contracts/Chunky.sol
+++ contract-examples/chunkies/contracts/Chunky.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.21;
 import "@rmrk-team/evm-contracts/contracts/implementations/premint/RMRKEquippablePreMint.sol";
 
 contract Chunky is RMRKEquippablePreMint {
+    mapping(address => bool) private _autoAcceptCollection;
+
     // Constructor
     constructor(
           string memory collectionMetadata,
@@ -34,6 +36,29 @@ contract Chunky is RMRKEquippablePreMint {
         addAssetToToken(tokenId, uint64(assetId), 0);
         // This implementation auto accepts first asset, if it weren't the case we could do:
         // _acceptAsset(tokenId, 0, uint64(assetId)); // This is an internal call which bypasses accepting by owner. Recommended only during minting.
+    }
+
+    function setAutoAcceptCollection(
+        address collection
+    ) public virtual onlyOwnerOrContributor {
+        _autoAcceptCollection[collection] = true;
+    }
+
+    function _afterAddChild(
+        uint256 tokenId,
+        address childAddress,
+        uint256 childId,
+        bytes memory
+    ) internal override {
+        // Auto accept children if they are from known collections
+        if (_autoAcceptCollection[childAddress]) {
+            _acceptChild(
+                tokenId,
+                _pendingChildren[tokenId].length - 1,
+                childAddress,
+                childId
+            );
+        }
     }
 }
   
