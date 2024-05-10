3487b6364b88034ae0ecaba1d7fc4669a197c798
Yaki (Shiba Tales) <83098539+shibatales@users.noreply.github.com>
2024-05-10 10:51:01 -0700

    Fix for Module not found: Can't resolve 'canvas'
    Linked to issue https://github.com/rmrk-team/rmrk-examples/issues/3

--- react-nextjs-example/next.config.js
+++ react-nextjs-example/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals = [ ...config.externals, "canvas", "jsdom" ];
+    return config;
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
